05c55b30fe31bd3321d92f05b87fd46e9c2c4ec2
ZainManzoor <zainmanzoor101@gmail.com>
2025-01-12 20:05:24 +0500

    Initial Commit for the react 18

---
+++ App.js
@@ -0,0 +1,20 @@
+// const element = React.createElement('h1', { id: 'firstHeading'}, 'Hello World;');
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+
+// another example for the nested application and siblings. 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const parent = React.createElement('div', { id: 'parent' }, 
+    [
+        React.createElement('h1', { id: 'firstHeading' }, 'Hello World'),
+        React.createElement('p', { id: 'firstParagraph' }, 'This is a paragraph', React.createElement('span', { id: 'firstSpan' }, 'This is a span')),
+    ],
+    [
+        React.createElement('h1', { id: 'firstHeading' }, 'Hello World'),
+        React.createElement('p', { id: 'firstParagraph' }, 'This is a paragraph', React.createElement('span', { id: 'firstSpan' }, 'This is a span')),
+    ], 
+
+);
+root.render(parent);
