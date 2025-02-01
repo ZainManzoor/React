b72193f990df7841dc3fb302c676b201b11f2fdd
ZainManzoor <zainmanzoor101@gmail.com>
2025-02-02 00:40:37 +0500

    Added few packages in the platform. Added React, Parcel, and gitIgnore file in the project.

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // const element = React.createElement('h1', { id: 'firstHeading'}, 'Hello World;');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(element);
